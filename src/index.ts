0bc420fba885f04810ac65a71a2a4eee7907193b
Gabriel Llamas <gabriel.llamas@erni-espana.es>
2017-05-05 15:41:01 +0200

    add result type definition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export default function(fn?: (...args: any[]) => any): (...args: any[]) => any {
-  if (!fn) return () => Promise.resolve();
+export default function<T>(
+  fn?: (...args: any[]) => T
+): (...args: any[]) => Promise<T> {
+  if (!fn) return () => Promise.resolve() as Promise<T>;
   // tslint:disable-next-line:only-arrow-functions
-  const pfn = async function(this: any, ...args: any[]): Promise<any> {
+  const pfn = async function(this: any, ...args: any[]): Promise<T> {
     return fn.apply(this, args);
   };
   return Object.defineProperty(pfn, 'name', { value: fn.name });
